e337387fb78499d4bb7ad856c8087cf4eebcff43
ankitabadhe <ankitabadhe340@gmail.com>
2023-02-19 21:35:03 +0530

    BRO

--- binary.cpp
+++ binary.cpp
@@ -1,62 +1,57 @@
- #include<iostream>
+#include <iostream>
 using namespace std;
 
-void printArray(int arr[], int size) {
-        int i;
-        for (i = 0; i < size; i++)
-                cout << arr[i] << " ";
-        cout << endl;
-}
-void bubble_sort(int A[], int n) {
-        int temp;
-        for (int k = 0; k < n - 1; k++) {
-                // (n-k-1) is for ignoring comparisons of elements which have already been compared in earlier iterations
-                for (int i = 0; i < n - k - 1; i++) {
-                        if (A[i] > A[i + 1]) {
-                                // here swapping of positions is being done.
-                                temp = A[i];
-                                A[i] = A[i + 1];
-                                A[i + 1] = temp;
-                        }
-
-                }
-                cout << "Pass" << k << " : ";
-                printArray(A, n);
+int binarySearch(int arr[], int n, int key) {
+    int left = 0, right = n - 1, mid;
+    while (left <= right) {
+        mid = (left + right) / 2;
+        if (arr[mid] == key) {
+            return mid;
         }
+        else if (arr[mid] > key) {
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    return -1;
 }
 
 int main() {
-        int n, i, j, search, first, last, middle, temp;
-        cout << "Enter total number of elements :";
-        cin >> n;
-        int arr[n];
-        for (int i = 0; i < n; i++) {
-                cout << "Enter element " << i + 1 << " : ";
-                cin >> arr[i];
-        }
+    int arr[100], n, key, choice, index;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+    cout << "Enter the elements of the array in sorted order: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    do {
+        cout << "\nBinary Search Operations:" << endl;
+        cout << "1. Search" << endl;
+        cout << "2. Quit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-        bubble_sort(arr, n);
-        cout << "Sorted array : ";
-        printArray(arr, n);
-        cout << "\nEnter a number to find :";
-        cin >> search;
-        first = 0;
-        last = n - 1;
-        middle = (first + last) / 2;
-        while (first <= last) {
-                if (arr[middle] < search) {
-                        first = middle + 1;
+        switch (choice) {
+        case 1:
+            cout << "Enter the key to search: ";
+            cin >> key;
+            index = binarySearch(arr, n, key);
+            if (index == -1) {
+                cout << key << " is not found in the array." << endl;
+            }
+            else {
+                cout << key << " is found at index " << index << " in the array." << endl;
+            }
+            break;
+        case 2:
+            cout << "Exiting..." << endl;
+            break;
+        default:
+            cout << "Invalid choice." << endl;
+        }
+    } while (choice != 2);
 
-                } else if (arr[middle] == search) {
-                        cout << search << " found at location " << middle + 1 << "\n";
-                        break;
-                } else {
-                        last = middle - 1;
-                }
-                middle = (first + last) / 2;
-        }
-        if (first > last) {
-                cout << "Not found! " << search << " is not present in the list.";
-        }
+    return 0;
 }
-
